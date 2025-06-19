0d14de6bc548eb38acd6c5420e5b6a482bb85802
Innovative <martyniak.innovative@gmail.com>
2025-06-19 16:09:30 +0200

    feat(ci): very early prototype

--- ci.ts
+++ ci.ts
@@ -1,7 +1,54 @@
-import { o, t } from "@nucleic/turtle";
+import { Color, o, t } from "@nucleic/turtle";
+import { readConfig } from "./core/config.ts";
+import { ServiceStatus } from "./src/services.ts";
+
+const URL = "127.0.0.1:9420";
 
 if (import.meta.main) {
+  const { name, version } = readConfig();
   console.clear();
   o("\nSilicon", "Nucleic", { ansi: "\[32m" }, "Array\n");
-  t("Starting to look around");
+  t("Looking around...");
+
+  const response = await snaConnect();
+
+  if (!response) {
+    t("Not accessible", { ansi: `\[${Color.red}m` });
+    Deno.exit();
+  }
+
+  if (response.name === name) {
+    const ws = new WebSocket(`ws://${URL}`);
+
+    ws.onopen = async () => {
+      t(`${response.version} array accesed`);
+      const services: ServiceStatus[] = await getServices();
+
+      t(`${services.length}`, "matters");
+      for (const service of services) {
+        o(
+          "-",
+          service.name,
+          service.installed
+            ? service.active ? "active" : "inactive"
+            : "not installed",
+        );
+      }
+    };
+    ws.onclose = () => {
+      t("conn closed");
+    };
+  }
+}
+
+export function getServices() {
+  return fetch(`http://${URL}/services`).then((res) => res.json()).catch(
+    (err) => {},
+  );
+}
+
+export function snaConnect() {
+  return fetch(`http://${URL}/info`).then((res) => res.json()).catch(
+    (err) => {},
+  );
 }
